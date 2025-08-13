95cdcee951575eab66fa18b9ccf814b288f9d49c
Sanchez Chandia Lautaro <57868867+sanchezchandialf@users.noreply.github.com>
2025-08-13 18:31:44 -0300

    feat: render navbar in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 import theme from './theme/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Navbar />
       <Home />
     </ThemeProvider>
   );

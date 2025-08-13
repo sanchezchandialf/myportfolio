fce329b094afa6d7974ce6e9953b12af52b1a257
Sanchez Chandia Lautaro <57868867+sanchezchandialf@users.noreply.github.com>
2025-08-13 10:20:09 -0300

    feat: add Material UI navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,28 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
 
 const Navbar = () => (
-  <AppBar position="static">
-    <Toolbar>
+  <AppBar
+    position="sticky"
+    color="transparent"
+    elevation={0}
+    sx={{ boxShadow: 'none' }}
+  >
+    <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
       <Typography variant="h6" component="div">
-        My Portfolio
+        Mi Portfolio
       </Typography>
+      <Box sx={{ display: 'flex', gap: 2 }}>
+        <Link href="#proyectos" color="inherit" underline="none">
+          Proyectos
+        </Link>
+        <Link href="#contacto" color="inherit" underline="none">
+          Contacto
+        </Link>
+      </Box>
     </Toolbar>
   </AppBar>
 );

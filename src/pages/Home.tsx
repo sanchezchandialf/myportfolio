15b2ea6b0211421fa8b93fefab0b8026ae357d64
Sanchez Chandia Lautaro <57868867+sanchezchandialf@users.noreply.github.com>
2025-08-16 15:43:17 -0300

    Add dynamic theming and layout components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from '../components/Hero';
+import About from '../components/About';
 import Projects from '../components/Projects';
 
 const Home = () => (
   <>
     <Hero />
+    <About />
     <Projects />
   </>
 );

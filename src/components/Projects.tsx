2ddb9e5338c4fb31ad1961bc54a36e6081963693
Sanchez Chandia Lautaro <57868867+sanchezchandialf@users.noreply.github.com>
2025-08-16 12:28:00 -0300

    Add projects anchor id

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ import projects from '../data/projects';
 import ProjectCard from './ProjectCard';
 
 const Projects = () => (
-  <Box sx={{ p: 4 }}>
+  <Box id="projects" sx={{ p: 4 }}>
     <Grid container spacing={2} justifyContent="center">
       {projects.map((project) => (
         <Grid item key={project.title} xs={12} sm={6} md={4}>

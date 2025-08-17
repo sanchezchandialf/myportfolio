d371f49abee67b19e72e8e749a1b0c15ed535984
Sanchez Chandia Lautaro <57868867+sanchezchandialf@users.noreply.github.com>
2025-08-17 20:40:32 -0300

    Create TechStack section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero';
 import About from '../components/About';
+import TechStack from '../components/TechStack';
 import Projects from '../components/Projects';
 
 const Home = () => (
   <>
     <Hero />
     <About />
+    <TechStack />
     <Projects />
   </>
 );
